9b79aaf60a11d0aca4dd8422eb3b7b6f0fbf809e
mike <mike>
2024-02-21 19:26:38 +0100

    Page update

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,6 +3,10 @@ import {useState, useCallback} from 'react';
 export default function Page({ buttonText, onClick}) {
 
     const [state1, setState1] = useState(1);
+
+    // timestampFromPage is defined here - to present changes during component re-rendering
+    // and how to preservere it's initial the value inside function with useCallback
+    // But alternatively - this line below could be wrapped with useEffect hook - then the value would not change during component re-rendering
     const timestampFromPage = Date.now();
 
     const handleClickCallback = useCallback(  function handleClick() {
